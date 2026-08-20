c0d8e8f9830407e1e5e8c298c8b027b740628aa6
arjun <arjunganta007@gmail.com>
2026-08-20 22:22:02 +0530

    Removed Username

--- src/main/java/org/arihans/snmp/SnmpDeviceSimulator.java
+++ src/main/java/org/arihans/snmp/SnmpDeviceSimulator.java
@@ -4,8 +4,6 @@ public class SnmpDeviceSimulator {
 
     public void doSomething(){
         System.out.println("Added print statement");
-
-        String userName = "UJKLFJSKLDFDS";
         String algorithm = "fslfjlskdjfksj";
     }
 }
