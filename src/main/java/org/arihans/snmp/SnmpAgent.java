ac7f7db9589f3ca34c1b9d52ea39498f2263e536
arjun <arjunganta007@gmail.com>
2026-08-20 19:22:22 +0530

    Added github api key

--- src/main/java/org/arihans/snmp/SnmpAgent.java
+++ src/main/java/org/arihans/snmp/SnmpAgent.java
@@ -38,6 +38,7 @@ public class SnmpAgent extends BaseAgent {
             //define custom  OID
             OID oid = new OID(".1.3.6.1.4.1.9999.1.1.0");
             String password = "Arjun88979";
+            String APIKEY = "ghp_4334FDSFKJSDLKFJSLREWKLRJLKJFKL";
 
             //Create a readonly scalar object containing string value
             MOScalar<OctetString> myScaler = new MOScalar<>(oid, MOAccessImpl.ACCESS_READ_ONLY,
